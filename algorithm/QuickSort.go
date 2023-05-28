4562861db6b70f4a642e0d63a579a58958f8377d
strawberry <strawberry@localhost.localdomain>
2023-05-28 10:23:13 -0400

    test quick sort

--- algorithm/QuickSort.go
+++ algorithm/QuickSort.go
@@ -1,1 +1,22 @@
-package algorithm
+package main
+
+func Quick(sli []int) []int {
+	if len(sli) < 1 {
+		return sli
+	}
+	r := 0
+	l := len(sli)
+	for i := 0; i < l; i++ {
+		if r > l {
+			break
+		}
+		if sli[r] > sli[l] {
+			sli[r], sli[l] = sli[l], sli[r]
+		}
+		r++
+		l--
+	}
+	Quick(sli[:r])
+	Quick(sli[l:])
+	return sli
+}
---
+++ algorithm/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	sli := []int{1, 23, 34, 5, 12, 1, 23, 2, 23, 5, 762, 1, 23, 4}
+	Quick(sli)
+	fmt.Print(sli)
+}
