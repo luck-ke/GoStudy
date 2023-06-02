ffce1072ecb672073640be6a99994fb88da915ff
strawberry <strawberry@localhost.localdomain>
2023-06-02 11:31:02 -0400

    implement lru

--- lru/lru.go
+++ lru/lru.go
@@ -1,11 +1,17 @@
 package lru
 
-import "container/list"
+import (
+	"container/list"
+)
 
 type LRUNode struct {
 	Caps int
 	Key  map[int]list.Element
 	list *list.List
+}
+
+type hash struct {
+	Key, Value int
 }
 
 type lRU struct {
@@ -19,6 +25,30 @@ func (lRU) New(caps int) *LRUNode {
 	}
 }
 
-func (lRU) Get() {
+func (node LRUNode) Get(key int) int {
+	value, ok := node.Key[key]
+	if ok {
+		node.list.MoveToFront(&value)
+		return value.Value.(hash).Value
+	}
+	return -1
+}
+
+func (node LRUNode) Set(key, value int) {
+	elem, ok := node.Key[key]
+	if ok {
+		elem.Value = hash{
+			Key:   key,
+			Value: value,
+		}
+		node.list.MoveToFront(&elem)
+	} else {
+		if node.Caps < node.list.Len() {
+			delete(node.Key, node.list.Back().Value.(hash).Value)
+			node.list.Remove(node.list.Back())
+		}
+		node.list.PushFront(hash{key, value})
+		node.Key[key] = *node.list.Front()
+	}
 
 }
