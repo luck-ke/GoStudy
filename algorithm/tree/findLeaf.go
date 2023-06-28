72f266e71febd564691540054cd8fb17ba23c831
strawberry <strawberry@localhost.localdomain>
2023-06-28 12:58:22 -0400

    tree

--- algorithm/tree/findLeaf.go
+++ algorithm/tree/findLeaf.go
@@ -1,7 +1,44 @@
 package tree
 
-func FindLeafs() {
+import "fmt"
 
+func FindLeafs(nodes *[]Node, id int) *[]*Node {
+	leaf := make([]*Node, 0)
+	idMap := make(map[int]*Node)
+	pidMap := make(map[int]struct{})
+	for _, v := range *nodes {
+		node := v
+		idMap[node.Id] = &node
+		pidMap[node.ParentId] = struct{}{}
+	}
+	fmt.Println(idMap)
+	fmt.Println(pidMap)
+	buds := []*Node{}
+	for _, v := range *nodes {
+		_, ok := pidMap[v.Id]
+		if ok {
+			n := v
+			buds = append(buds, &n)
+		}
+	}
+	fmt.Println(len(buds))
+	for _, v := range buds {
+		leaf = append(leaf, getLeaf(nodes, &idMap, &pidMap, v))
+	}
+	return &leaf
+}
+
+func getLeaf(nodes *[]Node, idMap *map[int]*Node, pidMap *map[int]struct{}, node *Node) *Node {
+	_, ok := (*pidMap)[(*node).Id]
+	if ok {
+		//return getLeaf(idMap, pidMap, (*idMap)[(*node).Id])
+		for _, n := range *nodes {
+			if n.ParentId == (*node).Id {
+				return getLeaf(nodes, idMap, pidMap, &n)
+			}
+		}
+	}
+	return (*idMap)[(*node).Id]
 }
 
 type Node struct {
--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-	"GoStudy/algorithm/dfs"
-	algorithnm "GoStudy/algorithm/sort"
+	"GoStudy/algorithm/tree"
 	"fmt"
 )
 
 func main() {
 	//sli := []int{1, 23, 34}
 
-	sli := []int{5, 23, 34, 5, 12, 1, 23, 2, 23, 5, 762, 1, 23, 4}
-	//testLen(sli[:7])
-	algorithnm.Quick(sli)
-	fmt.Println(sli)
+	//sli := []int{5, 23, 34, 5, 12, 1, 23, 2, 23, 5, 762, 1, 23, 4}
+	////testLen(sli[:7])
+	//algorithnm.Quick(sli)
+	//fmt.Println(sli)
+	//
+	//fmt.Println(dfs.Parenthesis(3))
 
-	fmt.Println(dfs.Parenthesis(3))
+	nodes := []tree.Node{
+		{Id: 1, ParentId: 0, Value: "Node 1"},
+		{Id: 2, ParentId: 1, Value: "Node 2"},
+		{Id: 3, ParentId: 1, Value: "Node 3"},
+		{Id: 4, ParentId: 2, Value: "Node 4"},
+		{Id: 5, ParentId: 2, Value: "Node 5"},
+		{Id: 6, ParentId: 3, Value: "Node 6"},
+		{Id: 7, ParentId: 3, Value: "Node 7"},
+		{Id: 8, ParentId: 4, Value: "Node 8"},
+		{Id: 9, ParentId: 4, Value: "Node 9"},
+	}
+	for _, v := range *tree.FindLeafs(&nodes, 0) {
+		fmt.Println(*v)
+	}
 }
 
 func testLen(s []int) {
